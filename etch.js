d219fad843925aff9366455bce93cd68aef5b45f
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-25 16:46:30 -0600

    hovering boxes toggles color

--- etch.js
+++ etch.js
@@ -1,7 +1,12 @@
 for (let col = 1; col < 17; col++) {
     for (let row = 1; row < 17; row++) {
         const div = document.createElement('div');
+        div.classList.add('box');
         div.textContent = col.toString() + row.toString();
         document.querySelector('.container').appendChild(div);
     }
 }
+
+boxes = document.querySelectorAll('div.box');
+boxes.forEach((box) => box.addEventListener('mouseover', 
+    () => box.classList.toggle('hoverEffect')));
