bc43961c033ebd8ae5b18ad0f9bb3e0dd1311da9
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-30 20:46:42 -0600

    shading feature added

--- etch.js
+++ etch.js
@@ -33,36 +33,56 @@ function clear() {
 function erase() {
     document.querySelector('button#erase').classList.add('inUse');
     document.querySelector('button#draw').classList.remove('inUse');
+    document.querySelector('button#tone').classList.remove('inUse');
 
     const boxes = document.querySelectorAll('div.box');
-    boxes.forEach(box => box.addEventListener('mouseover', () => 
-        box.classList.remove('hoverEffect'), {once : true}));
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.classList.remove('hoverEffect');
+        box.classList.add('erased');
+        box.style.color = 'white';
+        box.style.opacity = 0;
+    }, {once : true}));
 }
 
 function draw() {
     document.querySelector('button#erase').classList.remove('inUse');
     document.querySelector('button#draw').classList.add('inUse');
+    document.querySelector('button#tone').classList.remove('inUse');
 
     const boxes = document.querySelectorAll('div.box');
-    boxes.forEach(box => box.addEventListener('mouseover', () => 
-        box.classList.add('hoverEffect'), {once : true}));
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.classList.remove('erased');
+        box.classList.add('hoverEffect');
+        box.style.opacity = 1;
+    }, {once : true}));
 }
 
-// function shadeChange() {
-//     const boxes = document.querySelectorAll('div.box');
-//     boxes.forEach(box => box.addEventListener('mouseover', () => {
-//         box.classList.remove('hoverEffect');
+function shadeChange() {
+    document.querySelector('button#draw').classList.remove('inUse');
+    document.querySelector('button#erase').classList.remove('inUse');
+    document.querySelector('button#tone').classList.add('inUse');
 
-//         const boxColor = window.getComputedStyle(box).backgroundColor;
-//         const newR = +boxColor.slice(4,5) + 17;
-//         const newG = +boxColor.slice(7,8) + 17;
-//         const newB = +boxColor.slice(10,11) + 17;
-//         box.style.backgroundColor = `rgb(${newR}, ${newB}, ${newG})`;
-//     }));
-// }
+    const boxes = document.querySelectorAll('div.box');
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.classList.remove('hoverEffect');
+        box.classList.remove('erased');
+    }, {once : true}));
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        if (!box.classList.contains('hoverEffect')) 
+        {
+            if (!box.classList.contains('erased')) {
+                box.style.backgroundColor = 'black';
+                const currentShade = box.style.opacity;
+                if (currentShade === "1" || currentShade === "") box.style.opacity = 0;
+                else box.style.opacity = parseFloat(currentShade) + .1;
+            }
+        }
+    }));
+}
 
 gridify(16);
 document.querySelector('button').addEventListener('click', resize);
 document.querySelector('button#clear').addEventListener('click', clear);
 document.querySelector('button#erase').addEventListener('click', erase);
 document.querySelector('button#draw').addEventListener('click', draw);
+document.querySelector('button#tone').addEventListener('click', shadeChange);
